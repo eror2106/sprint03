b210fb0e91c4273ae73fa0735fdc87b503f4322a
ΞЯЯФЯ <49128357+eror2106@users.noreply.github.com>
2022-02-10 18:44:00 +0400

    update by error

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 let liste_utilisateur = [];
-let nb_ligne = 0;
+let tr_personne;
+let td_btn_griser;
+let span;
 let id = 0;
 let indice_liste_utiliateur = 0;
 let td_nom;
 let td_date;
 let tableau = document.getElementById("tableau");
+let boutton;
 function recherche_id() {
   id++;
   return id;
@@ -45,12 +48,12 @@ function ajouterLigne() {
   liste_utilisateur.push(persone);
   //creation des elements
   tableau = document.getElementById("tableau"); //recuperation du tableau
-  let tr_personne = document.createElement("tr"); //parent
+  tr_personne = document.createElement("tr"); //parent
   td_nom = document.createElement("td"); //enfant
   td_date = document.createElement("td"); //enfant
-  let td_btn_griser = document.createElement("td"); //enfant
-  let boutton = document.createElement("button"); //enfant d'enfants
-  let span = document.createElement("span");
+  td_btn_griser = document.createElement("td"); //enfant
+  boutton = document.createElement("button"); //enfant d'enfants
+  span = document.createElement("span");
   //ajjout de class
   tr_personne.className = "ligne";
   td_nom.className = "cellule_persone";
@@ -94,64 +97,11 @@ function get_id_btn(textContent) {
   return tabid[tabid.length - 1];
 }
 
-function delete_row(r) {
-  let nom = document.getElementsByClassName("cellule_persone");
-  let idbouton;
-  let ligne = document.getElementsByClassName("nom_mort");
-  let rowCount = ligne.length;
-  console.log(rowCount);
-  for (let index = 0; index < rowCount; ++index) {
-    idbouton = get_id_btn(ligne[index].textContent);
-    for (let id = 0; id < liste_utilisateur.length; id++) {
-      if (liste_utilisateur[id].id == idbouton) {
-        console.log("ici");
-        console.log("id objet" + liste_utilisateur[id].id);
-        console.log("idbouton" + idbouton);
-        console.log("nom" + nom.item(0).textContent);
-        tableau.deleteRow(idbouton);
-      }
-    }
+function delete_row() {
+  let lignes = document.getElementsByClassName("nom_mort");
+
+  while (0 < lignes.length) {
+    const current_ligne = lignes.item(0);
+    current_ligne.parentNode.removeChild(current_ligne);
   }
-}
-function suivant() {
-  var j = 0;
-  var table_user = document.getElementById("tableau");
-  var tailleListe = liste_utilisateur.length; // Recuperation de la taille de la liste
-
-  // Permet d'effacer en mode file
-  var tableHeaderRowCount = 1;
-  var table = document.getElementById("tableau");
-  var rowCount = table.rows.length;
-  table.deleteRow(1);
-
-  console.log("participant", liste_utilisateur[j]);
-  liste_utilisateur.push(liste_utilisateur[j]);
-
-  // **
-  // Je vais refaire une ligne dans la table
-
-  var table_user = document.getElementById("tableau");
-  var tr_user = document.createElement("tr");
-
-  var td_user_nom = document.createElement("td");
-  var td_user_time = document.createElement("td");
-  var td_user_bouton = document.createElement("td");
-
-  tr_user.className = "tr_demandant_base1";
-  td_user_nom.className = "td_nom_demandant_base";
-  td_user_time.className = "td_time_demandant_base";
-
-  td_user_nom.textContent = liste_utilisateur[j].nom;
-  td_user_time.textContent = liste_utilisateur[j].date1;
-
-  table_user.appendChild(tr_user);
-  tr_user.appendChild(td_user_nom);
-  tr_user.appendChild(td_user_time);
-  tr_user.appendChild(td_user_bouton);
-
-  //console.log(table_user);
-  liste_utilisateur.splice(0, 1);
-  console.log("participant apres slice", liste_utilisateur);
-
-  j++;
 }
